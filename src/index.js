1a2554c3442473c301cc4b7929fa4264dc760406
SunRay1017 <13208295650@163.com>
2023-04-03 23:36:15 +0800

    第一次提交

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 const program = require("commander");
-const pkg = require("./package.json");
+const pkg = require("../package.json");
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 const figlet = require("figlet");
 const fs = require("fs");
-const path=require("path")
+const path = require("path");
 // 下载
 const download = require("download-git-repo");
 const ora = require("ora");
@@ -22,15 +22,51 @@ program
         name: "name",
         message: "请输入您的项目名称",
       },
+      {
+        type: "input",
+        name: "name",
+        message: "请输入项目描述",
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "请输入项目作者",
+      },
+      {
+        type: "list",
+        message: "请选择JavaScript框架?",
+        name: "iframe",
+        choices: ["React", "Vue"],
+      },
+      {
+        type: "list",
+        message: "请选择开发语言?",
+        name: "lang",
+        choices: ["TypeScript", "JavaScript"],
+      },
+      {
+        type: "confirm",
+        message: "是否开启项目规范？",
+        name: "rules",
+      },
     ]);
+    console.log("answer", answer);
     // 出现加载图标
     let projectName = answer.name;
     const spinner = ora("Downloading...");
-    const url = "SunRay1017/xyl-react-template";
+    let url = "";
+    const { iframe, lang, rules } = answer;
+    if (iframe === "React" && lang === "TypeScript" && rules) {
+      url = "SunRay1017/xyl-react-template";
+    } else {
+      console.log("暂无模板！")
+      return;
+    }
+
     spinner.text = "疯狂加载中";
     spinner.color = "green";
     spinner.start();
-    download(url, projectName,(err) => {
+    download(url, projectName, (err) => {
       if (err) {
         spinner.fail();
         console.log(chalk.red(`Generation failed. ${err}`));
@@ -44,30 +80,15 @@ program
       const package = require(packageFile);
       // package.description = description;
       package.name = projectName;
-      
-      
-      (
-        packageFile,
+
+      packageFile,
         `module.exports = ${JSON.stringify(package, null, "\t")};`,
-        "utf8"
-      );
+        "utf8";
       // 结束加载图标
       spinner.succeed();
       console.log(chalk.green("\n Generation completed!"));
       console.log("\n To get started");
       console.log(`\n    cd ${projectName} \n`);
     });
-    // console.log(chalk.blue.bgRed.bold(answer.name));
-
-    // console.log(
-    //   "\r\n" +
-    //     figlet.textSync("xyl", {
-    //       font: "Ghost",
-    //       horizontalLayout: "default",
-    //       verticalLayout: "default",
-    //       width: 80,
-    //       whitespaceBreak: true,
-    //     })
-    // );
   });
 program.parse(process.argv);
